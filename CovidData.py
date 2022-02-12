c2762bc2262936c02a24375707a7ecc21330b1d2
Joshua Sessanga <Jasess04@gmail.com>
2022-02-12 16:08:55 -0500

    figure one with the top 20 countries with the most confirmed cases

--- CovidData.py
+++ CovidData.py
@@ -3,3 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px ### for plotting the data on world map
+
+path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/02-11-2022.csv'
+df = pd.read_csv(path)
+df.info()
+df.head()
+
+df.drop(['FIPS', 'Admin2','Last_Update','Province_State', 'Combined_Key'], axis=1, inplace=True)
+df.rename(columns={'Country_Region': "Country"}, inplace=True)
+df.head()
+
+### group the data by country
+
+world = df.groupby("Country")['Confirmed','Active','Recovered','Deaths'].sum().reset_index()
+world.head()
+
+### Find top 20 countries with maximum number of confirmed cases
+top_20 = world.sort_values(by=['Confirmed'], ascending=False).head(20)
+### Generate a Barplot
+plt.figure(figsize=(12,10))
+plot = sns.barplot(top_20['Confirmed'], top_20['Country'])
+for i,(value,name) in enumerate(zip(top_20['Confirmed'],top_20['Country'])):
+  plot.text(value,i-0.05,f'{value:,.0f}',size=10)
+plt.show()
+
+top_5 = world.sort_values(by=['Confirmed'], ascending=False).head()
